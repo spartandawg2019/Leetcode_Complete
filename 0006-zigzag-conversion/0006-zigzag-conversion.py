92e544ac4747a7805d3101442a84a5cb37f5a800
Tom K <57651387+spartandawg2019@users.noreply.github.com>
2023-05-08 17:51:06 -0400

    Time: 61 ms (62.68%), Space: 16.4 MB (22.26%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,14 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+        result = [''] * numRows
+        index, step = 0, 1
+        for char in s:
+            result[index] += char
+            if index == 0:
+                step = 1
+            elif index == numRows - 1:
+                step = -1
+            index += step
+        return ''.join(result)
