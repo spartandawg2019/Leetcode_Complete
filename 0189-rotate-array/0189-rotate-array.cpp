328b245e2b62e5926b30ffcc527664687944a9f4
Tom K <57651387+spartandawg2019@users.noreply.github.com>
2023-05-10 18:00:13 -0400

    Time: 45 ms (5.89%), Space: 24.9 MB (93.87%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;
+        if (k == 0) return;
+        int cnt = 0;
+        for (int start = 0; cnt < n; start++) {
+            int current = start;
+            int prev = nums[start];
+            do {
+                int next = (current + k) % n;
+                int temp = nums[next];
+                nums[next] = prev;
+                prev = temp;
+                current = next;
+                cnt++;
+            } while (start != current);
+        }
+    }
+};
