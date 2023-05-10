939be6c820fe159cedeaecadd95026538faff7bc
Tom K <57651387+spartandawg2019@users.noreply.github.com>
2023-05-10 12:56:50 -0400

    Time: 39 ms (72.52%), Space: 18.9 MB (85.61%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if (intervals.empty()) return {};
+        sort(intervals.begin(), intervals.end(), [](auto& a, auto& b) {
+            return a[0] < b[0];
+        });
+        vector<vector<int>> res;
+        res.push_back(intervals[0]);
+        for (int i = 1; i < intervals.size(); i++) {
+            if (res.back()[1] >= intervals[i][0]) {
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
+            } else {
+                res.push_back(intervals[i]);
+            }
+        }
+        return res;
+    }
+};
