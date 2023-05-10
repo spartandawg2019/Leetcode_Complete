f232baa49e74258de9e433c365aff58251d56c42
Tom K <57651387+spartandawg2019@users.noreply.github.com>
2023-05-10 11:13:48 -0400

    Time: 8 ms (29.23%), Space: 13.7 MB (63.04%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        backtrack(ans, "", 0, 0, n);
+        return ans;
+    }
+
+private:
+    void backtrack(vector<string>& ans, string cur, int open, int close, int max) {
+        if (cur.length() == 2*max) {
+            ans.push_back(cur);
+            return;
+        }
+        if (open < max) {
+            backtrack(ans, cur+"(", open+1, close, max);
+        }
+        if (close < open) {
+            backtrack(ans, cur+")", open, close+1, max);
+        }
+    }
+};
