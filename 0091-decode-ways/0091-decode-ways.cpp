27355677c17e0a124e4901b6b9afd8637ad3a7f5
Tom K <57651387+spartandawg2019@users.noreply.github.com>
2023-05-10 13:48:52 -0400

    Time: 5 ms (28.44%), Space: 6.2 MB (50.33%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+int numDecodings(string s) {
+int n = s.length();
+if (n == 0 || s[0] == '0') return 0;
+vector<int> dp(n + 1);
+dp[0] = 1;
+dp[1] = 1;
+for (int i = 2; i <= n; i++) {
+if (s[i - 1] != '0') dp[i] += dp[i - 1];
+if (s[i - 2] == '1' || (s[i - 2] == '2' && s[i - 1] <= '6')) dp[i] += dp[i - 2];
+}
+return dp[n];
+}
+};
