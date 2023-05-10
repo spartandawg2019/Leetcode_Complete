edff0cd1bea4ab35c54ecd1768170d30c51c7586
Tom K <57651387+spartandawg2019@users.noreply.github.com>
2023-05-10 11:24:27 -0400

    Time: 4 ms (72.66%), Space: 7.2 MB (72.6%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n = s.length();
+        int maxLen = 0;
+        stack<int> st;
+        st.push(-1);
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '(') {
+                st.push(i);
+            } else {
+                st.pop();
+                if (!st.empty()) {
+                    maxLen = max(maxLen, i - st.top());
+                } else {
+                    st.push(i);
+                }
+            }
+        }
+        return maxLen;
+    }
+};
